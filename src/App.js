36b25048f2aaeb392aae7b61e44a25724179d58f
dave cen <davecen9@gmail.com>
2023-11-24 16:08:18 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 
+import Button from "./Button";
+
 function App() {
-    return <div>App </div>
+    return <div>
+        <div>
+            <Button primary>Click!</Button>
+        </div>
+        <div>
+            <Button>By now!</Button>
+        </div>
+        <div>
+            <Button>See Deal!</Button>
+        </div>
+        <div>
+            <Button>Hide Ads!</Button>
+        </div>
+        <div>
+            <Button>Test!</Button>
+        </div>
+        <div>
+            <Button>Test2</Button>
+        </div>
+        <div>
+            <Button>Test3</Button>
+        </div>
+
+         </div>
 };
 
 export default App;
