7cc389905463cabecf3f61e37e06393ffd25bf8b
dave cen <davecen9@gmail.com>
2023-11-25 16:51:34 -0500

    save

--- src/Button.js
+++ src/Button.js
@@ -9,7 +9,9 @@ function Button({children,
      outline, 
      rounded}){
 
-    return <button>{children}</button>;
+
+
+    return <button className='px-3 py-1.5 border border-blue-500 bg-blue-500 text-white'>{children}</button>;
 }
 
 
