b5df418d963a2c59a431430b57d78cac7a62c168
dave cen <davecen9@gmail.com>
2023-12-17 15:25:18 -0500

    save

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,22 @@
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import {useState} from 'react';
+
+function ModalPage(){
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () =>{
+        setShowModal(true);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleClick} primary>Open Modal</Button> 
+            {showModal && <Modal/>}
+        </div>
+    );
+
+};
+
+export default ModalPage;
