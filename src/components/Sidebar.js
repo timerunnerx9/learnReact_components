fcfdb11713f1accaffa249ee7653f60f6b50ac9e
trx2 <davecen9@gmail.com>
2023-12-18 22:32:18 -0500

    save

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,8 @@ function Sidebar(){
         {label: "Dropdown", path:"/"}, 
         {label: "Accordion", path:"/accordion"}, 
         {label: "Buttons", path:"/buttons"},
-        {label: "Modal", path:"/modal"}
+        {label: "Modal", path:"/modal"},
+        {label: "Table", path:"/table"}
 
     ];
 
